810f52e72cc748a7f519b333c9f1db3fcdf651e1
mahmoudbadr2 <126113674+mahmoudbadr2@users.noreply.github.com>
2024-03-10 15:16:53 +0200

    add splash view

---
+++ lib/Features/splash/presentation/views_model/views/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/splash/presentation/views_model/views/widgets/splash_veiw_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
