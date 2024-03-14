f22dce0b2c1a36dcf4ba502827ff2174a6241e28
mahmoudbadr2 <126113674+mahmoudbadr2@users.noreply.github.com>
2024-03-14 03:58:30 +0200

    add custom scroll view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -13,66 +13,74 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * .25),
-              child: const CustomBookImage(),
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            const Text(
-              'The Jungle Book',
-              style: Styles.teztstyle30,
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            Opacity(
-              opacity: 0.7,
-              child: Text(
-                'Rudyard Kipling',
-                style: Styles.teztstyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .25),
+                  child: const CustomBookImage(),
                 ),
-              ),
+                const SizedBox(
+                  height: 35,
+                ),
+                const Text(
+                  'The Jungle Book',
+                  style: Styles.teztstyle30,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'Rudyard Kipling',
+                    style: Styles.teztstyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 40,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.teztstyle14
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 14,
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 18,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const BooksAction(),
-            const SizedBox(
-              height: 40,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Styles.teztstyle14.copyWith(fontWeight: FontWeight.w600),
-              ),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            const SimilarBooksListView(),
-            const SizedBox(
-              height: 14,
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
